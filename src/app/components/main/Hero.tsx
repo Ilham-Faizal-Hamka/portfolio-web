221a868a38937fec786b42323ceefe6aa5232979
Ilham-Faizal-Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2025-02-09 14:00:33 +0700

    fix: fix bug for video

--- src/app/components/main/Hero.tsx
+++ src/app/components/main/Hero.tsx
@@ -4,7 +4,7 @@ import HeroContent from '../sub/HeroContent'
 const Hero = () => {
   return (
     <div className='relative flex flex-col h-full w-full'>
-      <video autoPlay muted loop className='h-screen absolute left-0 z-[1] w-full h-full object-cover'>
+      <video autoPlay muted loop className='h-screen absolute left-0 z-[1] w-full object-cover'>
         <source src='/linedot.webm' type='video/webm'/>
       </video>
       <HeroContent/>

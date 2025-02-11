1f1992f195a72a063735da2d031d6b6932f99563
Ilham-Faizal-Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2025-02-11 18:18:26 +0700

    feat: footer

--- src/app/components/main/Footer.tsx
+++ src/app/components/main/Footer.tsx
@@ -7,31 +7,37 @@ import {
   RxLinkedinLogo,
 } from "react-icons/rx";
 
-import { FaYoutube } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+
+import { FaMedium } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <div className="w-full h-full bg-black text-gray-200 shadow-lg p-[15px] pt-20">
         <div className="w-full flex flex-col items-center justify-center m-auto px-36">
-            <div className="w-full h-full flex flex-row items-center justify-between flex-wrap">
+            <div className="w-full h-full flex flex-row items-center flex-wrap gap-10">
                 
 
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Community</div>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
+                <div className="min-w-[100px] h-auto flex flex-col items-start justify-start">
+                    <div className="font-bold text-[20px]">Community</div>
+                    <p className="flex flex-row items-center my-[10px] cursor-pointer">
+                        <RxLinkedinLogo className="text-3xl"/>
                         <span className="text-[15px] ml-[6px]">Youtube</span>    
                     </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxGithubLogo />
+                    <p className="flex flex-row items-center my-[10px] cursor-pointer">
+                        <RxGithubLogo className="text-3xl"/>
                         <span className="text-[15px] ml-[6px]">Github</span>    
                     </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxDiscordLogo />
+                    <p className="flex flex-row items-center my-[10px] cursor-pointer">
+                        <FaMedium className="text-3xl"/>
+                        <span className="text-[15px] ml-[6px]">Discord</span>    
+                    </p>
+                    <p className="flex flex-row items-center my-[10px] cursor-pointer">
+                        <FaWhatsapp className="text-3xl"/>
                         <span className="text-[15px] ml-[6px]">Discord</span>    
                     </p>
                 </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+                <div className="min-w-[200px] h-auto flex flex-col items-start justify-start">
                     <div className="font-bold text-[16px]">About</div>
                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
                      

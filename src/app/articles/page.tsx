a82a236a668d99fe7ea48121ccb73f8c2413262a
Ilham-Faizal-Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2025-02-19 00:39:28 +0700

    feat: create articles page under cunstruction

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,8 +1,17 @@
+'use client'
 import React from 'react'
+import ParticlesComponent from '../components/sub/ParticlesComponent'
 
 const Articles = () => {
   return (
-    <div>Articles</div>
+    <div className="relative flex h-screen w-full overflow-hidden items-center justify-center">
+      <div className='absolute h-full w-full z-[-1]'>
+      <ParticlesComponent/>
+      </div>
+      <div className='card flex items-center justify-center text-[40px] font-bold'>
+          <div className=" text-white text-center drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]">THE SITE IS UNDER CONSTRUCTION</div>
+      </div>
+    </div>
   )
 }
 

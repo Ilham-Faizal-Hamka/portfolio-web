147d6e00b660f8c344320672143e2e25927b1f0b
Ilham-Faizal-Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2025-02-20 10:58:27 +0700

    feat: contact page fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -70,11 +70,23 @@ const Contact = () => {
               className='h-[200px]'
               placeholder='Type your message here...'
               />
-              <Button className='max-w-40'>Send Message</Button>
+              <Button size="md" className='max-w-40'>Send Message</Button>
             </form>
           </div>
           <div className='flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0'>
-
+            <ul>
+              {info.map((item, index) => (
+                <li key={index} className='flex items-center gap-6'>
+                  <div className='w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-[#9ffeff] rounded-md flex items-center justify-center'>
+                    <div className='text-[28px]'>{item.icon}</div>
+                  </div>
+                  <div className='flex-1'>
+                    <p className='text-white/60'>{item.label}</p>
+                    <h3 className='text-xl'>{item.value}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,16 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+		container: {
+			center: true,
+			padding: "15px",	
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "960px",
+			xl: "1280px",
+		},
   	extend: {
   		backgroundImage: {
   			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',

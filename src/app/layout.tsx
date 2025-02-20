474e7ff468e3a74ace8353e9e5f20cf7495b0ef7
Ilham-Faizal-Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2025-02-21 00:46:14 +0700

    refactoring

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,30 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from './components/main/NavBar'
-import Footer from './components/main/Footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/main/NavBar";
+import Footer from "./components/main/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Faizal Hamka dev',
-  description: 'This is Faizal hamka\'s portfolio website',
-}
+  title: "Faizal Hamka dev",
+  description: "This is Faizal hamka's portfolio website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col bg-[#030014] overflow-y-scrool overflow-x-hidden`}>
+      <body
+        className={`${inter.className} flex flex-col bg-[#030014] overflow-y-scrool overflow-x-hidden`}
+      >
         <Navbar />
-        <main className='flex-grow'>
-        {children}
-        </main>
+        <main className="flex-grow">{children}</main>
         <Footer />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import Image from 'next/image'
-import Hero from './components/main/Hero'
-import Experience from './components/main/Experience'
+import Image from "next/image";
+import Hero from "./components/main/Hero";
+import Experience from "./components/main/Experience";
+import { GoogleGeminiEffect } from "@/components/ui/google-gemini-effect";
+import { GoogleGeminiEffectDemo } from "./components/main/Connect";
 
 export default function Home() {
   return (
-   <main className='h-full w-full gap-20 flex flex-col'>
+    <main className="h-full w-full gap-20 flex flex-col">
       <Hero />
       <Experience />
-   </main>
-  )
+      <GoogleGeminiEffectDemo />
+    </main>
+  );
 }

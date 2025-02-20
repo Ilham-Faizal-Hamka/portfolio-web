ba0466dd0a3ca3a6e7dc0e6589da6a0e86168300
Ilham-Faizal-Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2025-02-20 17:58:04 +0700

    feat: fixing footer

--- src/app/components/main/Footer.tsx
+++ src/app/components/main/Footer.tsx
@@ -1,65 +1,71 @@
 import React from "react";
-import {
-  RxDiscordLogo,
-  RxGithubLogo,
-  RxInstagramLogo,
-  RxTwitterLogo,
-  RxLinkedinLogo,
-} from "react-icons/rx";
+import { RxGithubLogo, RxLinkedinLogo } from "react-icons/rx";
 
 import { FaWhatsapp } from "react-icons/fa";
 
 import { FaMedium } from "react-icons/fa6";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="w-full h-full bg-black text-gray-200 shadow-lg p-[15px] pt-20">
-        <div className="w-full flex flex-col items-center justify-center m-auto px-36">
-            <div className="w-full h-full flex flex-row items-center flex-wrap gap-10">
-                
-
-                <div className="min-w-[100px] h-auto flex flex-col items-start justify-start">
-                    <div className="font-bold text-[20px]">Community</div>
-                    <p className="flex flex-row items-center my-[10px] cursor-pointer">
-                        <RxLinkedinLogo className="text-3xl"/>
-                        <span className="text-[15px] ml-[6px]">Youtube</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[10px] cursor-pointer">
-                        <RxGithubLogo className="text-3xl"/>
-                        <span className="text-[15px] ml-[6px]">Github</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[10px] cursor-pointer">
-                        <FaMedium className="text-3xl"/>
-                        <span className="text-[15px] ml-[6px]">Discord</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[10px] cursor-pointer">
-                        <FaWhatsapp className="text-3xl"/>
-                        <span className="text-[15px] ml-[6px]">Discord</span>    
-                    </p>
-                </div>
-                <div className="min-w-[200px] h-auto flex flex-col items-start justify-start">
-                    <div className="font-bold text-[16px]">About</div>
-                   <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                     
-                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                      
-                        <span className="text-[15px] ml-[6px]">Learning about me</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                  
-                        <span className="text-[15px] ml-[6px]">mifwebchain@gmail.com</span>    
-                    </p>
-                </div>
+    <div className="w-full h-full bg-black text-gray-200 shadow-lg p-[15px] mt-6 pt-10">
+      <div className="w-full flex flex-col items-center justify-center m-auto px-36">
+        <div className="w-full h-full flex xl:flex-row flex-col items-start gap-5 justify-start xl:justify-between">
+          <div className="min-w-[100px] xl:order-2 h-auto flex flex-col items-start">
+            <div className="font-bold text-[20px]">Community</div>
+            <div className="flex flex-row gap-4">
+              <Link
+                href="https://www.linkedin.com/in/ilham-faizal-hamka/"
+                target="_blank"
+                className="flex flex-row items-center my-[10px] cursor-pointer"
+              >
+                <RxLinkedinLogo className="text-3xl" />
+                <span className="text-[15px] ml-[6px]">Linkedin</span>
+              </Link>
+              <Link
+                href="https://github.com/Ilham-Faizal-Hamka"
+                target="_blank"
+                className="flex flex-row items-center my-[10px] cursor-pointer"
+              >
+                <RxGithubLogo className="text-3xl" />
+                <span className="text-[15px] ml-[6px]">Github</span>
+              </Link>
             </div>
-
-            <div className="my-[20px] text-[15px] text-center">
-                &copy; Faizal Hamka Dev 2025 Inc. All rights reserved
+            <div className="flex flex-row gap-4">
+              <Link
+                href="https://medium.com/@faizalhamka45.dev"
+                className="flex flex-row items-center my-[10px] cursor-pointer"
+              >
+                <FaMedium className="text-3xl" />
+                <span className="text-[15px] ml-[6px]">Medium</span>
+              </Link>
+              <Link
+                href="https://wa.me/6281337863325"
+                target="_blank"
+                className="flex flex-row items-center my-[10px] cursor-pointer"
+              >
+                <FaWhatsapp className="text-3xl" />
+                <span className="text-[15px] ml-[6px]">Whatsapp</span>
+              </Link>
             </div>
+          </div>
+          <div className="max-w-[600px] xl:order-1 h-auto flex flex-col items-start justify-start">
+            <div className="font-bold text-[20px]">Faizal Hamka</div>
+            <p className="text-white text-[15px]">
+              Open for discussion! Feel free to reach out via social media on
+              the side. Stay updated with my latest insights on programming
+              through my Medium and this blog. I&apos;m ready to help you find
+              the best solutions for your needs!
+            </p>
+          </div>
         </div>
-    </div>
-  )
-}
 
-export default Footer
+        <div className="my-[20px] text-[15px] text-center">
+          &copy; Faizal Hamka Dev 2025 Inc. All rights reserved
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
